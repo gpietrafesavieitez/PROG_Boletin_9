61c343cf2b2101ef315b5cc671b0205fd31c1160
gpietrafesavieitez <gpietrafesavieitez@nautilus31.danielcastelao.org>
2018-11-12 11:59:37 +0100

    Refactorizado

--- Boletin_9_6/src/boletin_9_6/Boletin_9_6.java
+++ Boletin_9_6/src/boletin_9_6/Boletin_9_6.java
@@ -3,36 +3,25 @@ import java.util.Scanner;
 
 public class Boletin_9_6{
     private static Scanner leer = new Scanner(System.in);
-
+    private static Trabajador worker = new Trabajador(1);//Para que no salga del bucle si se produce una excepcion nada mas empezar
+    
     public static void main(String[] args){
-        int porcentaje = 0;
-        int cantidad = 0;
-
-        Trabajador worker = new Trabajador("prueba",1); //De momento sueldo debe ser 1 para que no salga del bucle al inicio
-        
+        double cantPobres = 0,cantRicos = 0;
         do{
             try{
-                System.out.print("Introduzca sueldo del trabajador (0 para finalizar)\n:");
+                System.out.print("\n[-]\tIntroduzca el sueldo (€) de un trabajador (0 para finalizar)\n\t:");
                 worker.setSueldo(Double.parseDouble(leer.nextLine()));
-                
                 if(worker.getSueldo() < 0){
-                    System.out.println("[!] Valor incorrecto: Debe ser un número positivo.");
+                    System.out.println("\n[!]\tValor incorrecto: Debe ser un número positivo.");
                 }else if(worker.getSueldo() > 0 && worker.getSueldo() < 1000){
-                    porcentaje++;
+                    cantPobres++;
                 }else if(worker.getSueldo() >= 1000 && worker.getSueldo() <= 1750){
-                    cantidad++;
+                    cantRicos++;
                 }
-                
             }catch(NumberFormatException e){
-                System.out.println("[!] Formato incorrecto: Debe ser un número.");
+                System.out.println("\n[!]\tFormato incorrecto: Debe ser un número.");
             }
-
         }while(worker.getSueldo() != 0);
-
-        System.out.println("Cantidad de trabajadores con sueldo menor a 1000: " + porcentaje);
-        System.out.println("Cantidad de trabajadores con sueldo entre 1000 y 1750: " + cantidad);
-        
-        
+        System.out.println("\n[*]\tCantidad de trabajadores con sueldo entre 1000€ y 1750€: " + (int) cantRicos + "\n\tPorcentaje de trabajadores con sueldo menor a 1000€: " + (cantPobres / (cantPobres + cantRicos) * 100) + "%");
     }
-    
 }
--- Boletin_9_6/src/boletin_9_6/Trabajador.java
+++ Boletin_9_6/src/boletin_9_6/Trabajador.java
@@ -1,32 +1,20 @@
 package boletin_9_6;
 
 public class Trabajador{
-    private String iden;
     private double sueldo;
     
     public Trabajador(){
     }
     
-    public Trabajador(String iden,double sueldo){
-        this.iden = iden;
+    public Trabajador(double sueldo){
         this.sueldo = sueldo;
-    }
-    
-    public String getIden(){
-        return iden;
     }
     
     public double getSueldo(){
         return sueldo;
     }
-    
-    public void setIden(String iden){
-        this.iden = iden;
-    }
-    
+
     public void setSueldo(double sueldo){
         this.sueldo = sueldo;
     }
-    
-   
 }
